bf94068afc142d0b5da71632de400899148b51c6
Oliver Kaut <oliver@Olivers-MacBook-Air.local>
2022-06-20 17:39:40 +0200

    created components, css grid and redux store

---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const Nav = () => {
+  return (
+    <nav className='navigation'>Nav</nav>
+  )
+}
+
+export default Nav
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
