551a4332a4c251ea89b0e56e8e295f42daa247d1
Oliver Kaut <oliver@Olivers-MacBook-Air.local>
2022-06-21 17:04:47 +0200

    Added Registration Modal

--- src/components/sidebar/SideBarMenus.tsx
+++ src/components/sidebar/SideBarMenus.tsx
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser } from "@fortawesome/free-solid-svg-icons"
-import React, { useEffect } from "react"
+import { faUser, faRegistered } from "@fortawesome/free-solid-svg-icons"
+import React, { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { AppState } from "../../store/AppState"
 import { UserProfileSetType } from "../../store/user/Reducer"
 import './SideBarMenus.css'
+import Registration from "../auth/Registration"
 
 
 const SideBarMenus = () => {
+    const [showRegister, setShowRegister] = useState(false)
     const user = useSelector((state: AppState) => state.user)
     const dispatch = useDispatch()
 
@@ -21,11 +23,22 @@ const SideBarMenus = () => {
         })
     }, [dispatch])
 
+    const onClickToggleRegister = () => {
+        setShowRegister(!showRegister)
+    }
+
     return (
         <React.Fragment>
             <ul>
-                <FontAwesomeIcon icon={faUser} />
-                <span className="menu-name">{user?.userName}</span>
+                <li>
+                    <FontAwesomeIcon icon={faUser} />
+                    <span className="menu-name">{user?.userName}</span>
+                </li>
+                <li>
+                    <FontAwesomeIcon icon={faRegistered} />
+                    <span className="menu-name" onClick={onClickToggleRegister}>register</span>
+                    <Registration isOpen={showRegister} onClickToggle={onClickToggleRegister} />
+                </li>
             </ul>
         </React.Fragment>
     )
