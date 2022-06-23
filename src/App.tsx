16a464198ccbbb36c72b0b439d697f74e86350fa
Oliver Kaut <oliver@Olivers-MacBook-Air.local>
2022-06-23 16:52:02 +0200

    Added Top Categories in right menu and started building Thread posts screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './components/routes/Home';
+import Thread from './components/routes/thread/Thread';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/categorythreads/:categoryId" element={<Home />} />
+      <Route path="/thread/:id" element={<Thread />} />
     </Routes>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import configureStore from './store/configureStore';
 import reportWebVitals from './reportWebVitals';
+import ReactModal from "react-modal";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -20,6 +21,8 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+ReactModal.setAppElement("#root");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
